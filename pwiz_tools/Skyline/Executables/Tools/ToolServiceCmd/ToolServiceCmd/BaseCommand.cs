6a5a5c4dc42752f352f8d5e49a9c4b729f4451af
Nicholas Shulman <nicksh@uw.edu>
2018-08-24 11:51:58 -0700

    Command line utility which talks to the Skyline tool service API.

---
+++ pwiz_tools/Skyline/Executables/Tools/ToolServiceCmd/ToolServiceCmd/BaseCommand.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+using SkylineTool;
+
+namespace ToolServiceCmd
+{
+    public abstract class BaseCommand
+    {
+        [Option(Required = true, HelpText = "Connection name from Skyline")]
+        public string ConnectionName { get; set; }
+
+        public abstract int PerformCommand();
+
+        protected ToolServiceClient GetSkylineToolClient()
+        {
+            return new ToolServiceClient(ConnectionName);
+        }
+    }
+}
